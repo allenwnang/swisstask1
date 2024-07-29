952deab096bd63080330dfde870e1a6483c963ea
allenwnang <allenwang09142016@gmail.com>
2024-07-29 16:27:48 +0000

    swisstask1completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [""], //Your private key starting with "0x"
+      accounts: ["0xbcb74fd9e22f0d0df5183210c18b5a0c3364661cbe77bc7bd2d6f8bda128816f"], //Your private key starting with "0x"
     },
   },
 };
